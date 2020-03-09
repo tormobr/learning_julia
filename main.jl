446373c35fb2e5c3988880b0ccf8872a8cac5f22
Tormod <tormod@forest.fruit>
2020-03-09 17:41:28 +0100

    some array stuff

--- main.jl
+++ main.jl
@@ -1,4 +1,28 @@
 # Hello world program
 print("Hello world\n")
 
+# Simple arrays
+a = ["hello", "this", "is", "a", "test"]
 
+
+for x in a
+    print(x, "\n")
+end
+
+for (i, x) in enumerate(a)
+    print(i, " ",  x, "\n")
+end
+
+# 2D arrays??
+a = [[1,2,3], [4,5,6],[7,8,9]]
+
+for row in a
+    for col in row
+        print(col, "\n")
+    end
+end
+
+
+for row in a
+    print(row, "\n")
+end
