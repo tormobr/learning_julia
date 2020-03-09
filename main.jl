0661683148049331da9badf625b8a02e3ba3a270
tormobr <tormod.brandshoi@gmail.com>
2020-03-09 17:59:56 +0100

    some string stuff and casting

--- main.jl
+++ main.jl
@@ -1,16 +1,16 @@
 # Hello world program
-print("Hello world\n")
+println("Hello world")
 
 # Simple arrays
 a = ["hello", "this", "is", "a", "test"]
 
 
 for x in a
-    print(x, "\n")
+    println(x)
 end
 
 for (i, x) in enumerate(a)
-    print(i, " ",  x, "\n")
+    println(i, " ",  x)
 end
 
 # 2D arrays??
@@ -18,24 +18,32 @@ a = [[1,2,3], [4,5,6],[7,8,9]]
 
 for row in a
     for col in row
-        print(col, "\n")
+        println(col)
     end
 end
 
 
 for row in a
-    print(row, "\n")
+    println(row)
 end
 
 # Sum of list
 for row in a
-    print(sum(row), "\n")
+    println(sum(row))
 end
 
 # List comprehensions 
 a = [i for i=0:10]
-print(a, "\n")
-print(sum(i for i=0:10), "\n")
+println(a)
+println(sum(i for i=0:10))
 
+# Some string stuff here..
 s = "hello this is a test"
-print(split(s), "\n")
+ss = split(s)
+ss2 = split(s, "e")
+println(ss)
+println(ss2)
+
+s = "1,2,3,4,5,6"
+sum_of_string = sum([parse(Int64, c) for c in split(s, ",")])
+println(sum_of_string)
