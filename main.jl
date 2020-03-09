2d38153aabad5b1fe5fad83ce3c3253a050601ee
tormobr <tormod.brandshoi@gmail.com>
2020-03-09 19:30:56 +0100

    some functions and stuff

--- main.jl
+++ main.jl
@@ -13,6 +13,10 @@ for (i, x) in enumerate(a)
     println(i, " ",  x)
 end
 
+# Julia arrays starts index at 1
+a = [1,2,4]
+println(a[1])
+
 # 2D arrays??
 a = [[1,2,3], [4,5,6],[7,8,9]]
 
@@ -48,4 +52,27 @@ println(ss2)
 s = "1,2,3,4,5,6"
 sum_of_string = sum([parse(Int64, c) for c in split(s, ",")])
 println(sum_of_string)
+
+
+# Latex unicode
+λ = 2
+α = 3
+β = 5
+println("λ * α * β = ", λ * α * β)
+
+# Functions
+function f(x)
+    return 10 * x
+end
+println("f(10) = ", f(10))
+
+# Function with variable number of args
+function varargs(args...)
+    return sum(args)
+end
+println("var args( 1,2,3,4,5,6) = ", varargs(1,2,3,4,5,6))
+
+
+# simple map example with lambda function
+println(map(x-> x^2, [1,2,3]))
 
